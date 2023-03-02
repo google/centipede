e60cb0a17ed07fa0206351f12984f5fe397e3408
Kostya Serebryany <kcc@google.com>
2023-03-01 17:20:16 -0800

    #Centipede add minimize_crash_test.sh to test --minimize_crash
    PiperOrigin-RevId: 513384731

---
+++ testing/minimize_crash_test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Copyright 2022 The Centipede Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Tests the --minimize_crash flag.
+
+set -eu
+
+source "$(dirname "$0")/../test_util.sh"
+
+CENTIPEDE_TEST_SRCDIR="$(centipede::get_centipede_test_srcdir)"
+
+centipede::maybe_set_var_to_executable_path \
+  CENTIPEDE_BINARY "${CENTIPEDE_TEST_SRCDIR}/centipede"
+
+centipede::maybe_set_var_to_executable_path \
+  TARGET_BINARY "${CENTIPEDE_TEST_SRCDIR}/testing/minimize_me_fuzz_target"
+
+WD="${TEST_TMPDIR}/WD"
+LOG="${TEST_TMPDIR}/log"
+
+# Prepare the crasher input.
+CRASHER="${TEST_TMPDIR}/crasher"
+echo -n '?f???u???z?' > "${CRASHER}"
+
+# Run minimization loop
+centipede::ensure_empty_dir "${WD}"
+"${CENTIPEDE_BINARY}" --binary="${TARGET_BINARY}" --workdir="${WD}" \
+  --minimize_crash="${CRASHER}" --seed=1 --num_runs=100000 \
+  2>&1 |tee "${LOG}"
+
+# Check that the log contains the 5-byte crash input.
+# The 3 middle bytes of it should be 'fuz'.
+# The minimization for this test target is not guaranteed to produce
+# some specific 5-byte input.
+centipede::assert_regex_in_file "Crasher: size: 5: .*fuz.*" "${LOG}"
+
+# Check that we actually have a 5-byte-long file in "${WD}/crashes".
+find "${WD}/crashes" -size 5c
+
+echo "PASS"
